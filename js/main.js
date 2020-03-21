813ba8182a0349f5b524cd6b52094242c2acfb28
Miguel del Mazo <migueldelmazo@gmail.com>
2020-03-21 13:23:48 +0100

    Add first favorites version

--- js/main.js
+++ js/main.js
@@ -1,54 +1,47 @@
 'use-strict';
 
-//VARIABLES
+//VARIABLES SOLO DE JS
 let tvShows = [];
+let favShows = [];
+
+// VARIABLES DE JS Y HTML
 const showsEl = document.querySelector('.js-tvShowDataList');
-//console.log(showsEl);
 
 //FUNCIONES
 //Me traigo la API
-const getApiData = function () {
-
+const getApiData = function() {
   fetch('http://api.tvmaze.com/search/shows?q=dexter') //aquí meto url de api que está especificada en documentación según me espefique en ella.
     .then(response => response.json())
     .then(data => {
       tvShows = data;
-      console.log(data);
-      paintTvShows();
-      // products = data.cart.items;
-      //console.log(products);
-      //paintProduct(); //ejecutamos aquí paint products porque es donde se ha generado el array products. y lo pongo debajo del console.log por la asincronía.
-
-      //console.log(data.cart.items); es para comprobar que me funciona y cómo recibo la info.
+      console.log(data); // es para comprobar que me funciona y cómo recibo la info
+      paintTvShows(); // ejecutamos aquí paint products porque es donde se ha generado el array products. y lo pongo debajo del console.log por la asincronía.
+      // las dos siguientes líneas son fake
+      favShows = tvShows.slice(0, 2);
+      paintFavShows();
     });
-
 };
 
-const getHTMLShows = function (data) {
+const getHTMLShows = function(data) {
   let accHTML = ''; // es vacío para que se reinicie cada vez que pase por aquí.
   accHTML += `<li class="js-tvShowDataItem1">`;
-  accHTML += `<img src=${data.show.image} class="js-ShowDataImage" alt="Carátula de ${data.show.name}" />`;
+  accHTML += `<img src=${data.show.image.medium} class="js-ShowDataImage" alt="Carátula de ${data.show.name}" />`;
   accHTML += `<h3 class="js-tvShowdataTitle">${data.show.name}</h3>`;
   accHTML += `</li>`;
   return accHTML; //ojo que no se te olvide el return porque sino no te lo pinta.
-}
+};
 
-
-const paintTvShows = function (tvShow) {
+const paintTvShows = function() {
   let accTvShow = '';
   for (let index = 0; index < tvShows.length; index++) {
     accTvShow += getHTMLShows(tvShows[index]);
   }
-  showsEl.value = accTvShow;
-  //listenBtn();
-}
+  showsEl.innerHTML = accTvShow;
+};
 
+const paintFavShows = function() {
+  // patri tienes que rellenar esta función
+};
 
-
-
-
-
-
-
-//EJECUTAMOS
+// ARRANCAMOS LA PÁGINA
 getApiData();
