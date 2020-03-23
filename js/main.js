49ec73cfc64f5914eba09a840a8b080de4474689
includepatriciamartinez <includepatriciamartinez@gmail.com>
2020-03-23 13:26:24 +0100

    Add default image and remove from favShowsList selected item

--- js/main.js
+++ js/main.js
@@ -15,22 +15,16 @@ const textShowEl = document.querySelector('.js-inputText');
 //Me traigo la API
 const getApiData = function () {
   const inputTextValue = textShowEl.value;
-  console.log(inputTextValue);
-  console.log('me han clickado y el evento es:', event);
   fetch('http://api.tvmaze.com/search/shows?q=' + inputTextValue) //aquí meto url de api que está especificada en documentación según me espefique en ella.
     .then(response => response.json())
     .then(data => {
       tvShows = data;
-      // console.log(data); // es para comprobar que me funciona y cómo recibo la info
-      paintTvShows(); // ejecutamos aquí paint products porque es donde se ha generado el array products. y lo pongo debajo del console.log por la asincronía.
-      // las dos siguientes líneas son fake
-      paintFavShows();
-
+      paintTvShows(); // ejecutamos aquí los paintLoQueSea porque es donde se ha generado el array products
+      paintFavShows(); //y lo pongo debajo del console.log por la asincronía.
     });
 };
 
 //función handler getApiData cuando clicko en Search:
-
 const listenClickSearchBtn = function () {
   const btnSearchEl = document.querySelector('.js-btn');
   btnSearchEl.addEventListener('click', getApiData);
@@ -40,7 +34,12 @@ const getHTMLShows = function (serie) {
   let accHTML = '';
   // es vacío para que se reinicie cada vez que pase por aquí.
   accHTML += `<li class="js-tvShowDataListItem" id="${serie.show.id}">`;
-  //accHTML += `<img src=${serie.show.image.medium} class="js-ShowDataImage" alt="Carátula de ${serie.show.name}" />`;
+  if (serie.show.image === null) {
+    accHTML += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?
+    text=TV" class="js-ShowDataImage" alt="Carátula de ${serie.show.name}" />`;
+  } else {
+    accHTML += `<img src=${serie.show.image.medium} class="js-ShowDataImage" alt="Carátula de ${serie.show.name}" />`;
+  }
   accHTML += `<h3>${serie.show.name}</h3>`;
   accHTML += `</li>`;
   return accHTML; //ojo que no se te olvide el return porque sino no te lo pinta.
@@ -53,29 +52,22 @@ const paintTvShows = function () {
   }
   showsEl.innerHTML = accTvShow;
   listenClickShowList();
-
 };
 
 //funcion handler shows:
 const handlerShowsList = function (event) {
   // console.log('me han clickado y el evento es:', event);
   // console.log('me han clickado y el evento es:', event.currentTarget, event.currentTarget.id);
-  // AQUÍ EMPIEZA TU CÓDIGO
   const tvShowClickedId = parseInt(event.currentTarget.id); //traduzco a numero con el parceInt
   const tvShowClickedIndex = tvShows.findIndex(tvShows => tvShows.show.id === tvShowClickedId);
   const tvFavClickedIndex = favShows.findIndex(favShows => favShows.show.id === tvShowClickedId);
-  console.log(tvShowClickedIndex, tvFavClickedIndex);
 
-  if (tvFavClickedIndex === -1) { // si no está en fav meto
-    // meto con push el tvShow clickado
-    // Patri tienes que escribir aquí una sola línea
+  if (tvFavClickedIndex === -1) { // si no está en fav meto con push el tvShow clickado
     favShows.push(tvShows[tvShowClickedIndex])
-    console.log(favShows); // para saber que te funciona el siguiente console tiene que [{...}]
-  } else {
-    favShows.splice(tvFavClickedIndex, 1); // si está en fav saco con splice
+    //console.log(favShows); // para saber que te funciona el siguiente console tiene que [{...}]
+  } else { // si está en fav saco con splice
+    favShows.splice(tvFavClickedIndex, 1);
   }
-
-  // AQUÍ ACABA TU CÓDIGO
   paintTvShows();
   paintFavShows();
   setInLocalStorage();
@@ -83,7 +75,6 @@ const handlerShowsList = function (event) {
 
 const listenClickShowList = function () {
   const showsListListenerEl = document.querySelectorAll('.js-tvShowDataListItem');
-  // console.log('Cuántos LI he encontrado:', showsListListenerEl)
   for (let index = 0; index < showsListListenerEl.length; index++) {
     showsListListenerEl[index].addEventListener('click', handlerShowsList);
   }
@@ -91,9 +82,14 @@ const listenClickShowList = function () {
 
 const getHTMLfavShows = function (serie) {
   let codeHTML = '';
-  codeHTML += `<li class="js-tvShowDataListFavItem">`;
+  codeHTML += `<li class="js-tvShowDataListFavItem" id="${serie.show.id}">`;
   codeHTML += `<h3>${serie.show.name}</h3>`;
-  //codeHTML += `<img src=${serie.show.image.medium} class="js-ShowDataImage" alt="Carátula de ${serie.show.name}" />`;
+  if (serie.show.image === null) {
+    codeHTML += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?
+    text=TV" class="js-ShowDataImage" alt="Carátula de ${serie.show.name}" />`;
+  } else {
+    codeHTML += `<img src=${serie.show.image.medium} class="js-ShowDataImage" alt="Carátula de ${serie.show.name}" />`;
+  }
   codeHTML += `</li>`;
   return codeHTML;
 };
@@ -108,12 +104,11 @@ const paintFavShows = function () {
 };
 
 //funcion handler favShows:
-const handlerFavList = function (event) {
-  console.log('me han clickado y el evento es:', event.currentTarget);
-  // aquí estoy borrando la primera serie de favoritos de manera fake
-  if (favShows.length) {
-    favShows.splice(0, 1);
-  }
+const handlerFavList = function (event) { //console.log('me han clickado y el evento es:', event.currentTarget);como test.
+  const favShowClickedId = parseInt(event.currentTarget.id); //traduzco a numero con el parceInt
+  const tvFavClickedIndex = favShows.findIndex(favShows => favShows.show.id === favShowClickedId);
+  favShows.splice(tvFavClickedIndex, 1);
+  //console.log(favShows); // para saber que te funciona el siguiente console tiene que [{...}]
   paintTvShows();
   paintFavShows();
   setInLocalStorage();
@@ -127,7 +122,6 @@ const listenClickFavList = function () {
 };
 
 //función local storage
-
 const setInLocalStorage = function () {
   const stringifyFavShows = JSON.stringify(favShows);
   localStorage.setItem('favShows', stringifyFavShows);
@@ -141,8 +135,6 @@ const getFromLocalStorage = function () {
   }
 };
 
-// ARRANCAMOS LA PÁGINA
-// getApiData();
+// ARRANCAMOS LA PÁGINA (ejecutamos funciones necesarias,por ejemplo // getApiData(); // paintFavShows();)
 getFromLocalStorage();
 listenClickSearchBtn();
-// paintFavShows();
